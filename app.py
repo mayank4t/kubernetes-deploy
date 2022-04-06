252e45b704c335a8a58fd9a34b0147dd6dd8b2b4
mayank4t <mayank.geit89@gmail.com>
2022-04-06 18:19:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,6 @@ import time
 app = Flask(__name__)
 @app.route("/")
 def hello():
-    return jsonify({"Time of Call": time.time()})
+    return jsonify({"Time": time.time()})
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
